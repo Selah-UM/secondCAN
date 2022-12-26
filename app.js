ef50e06dced97ebd0e6fe5ac2afa754084f47b08
selah2 <konomi_20n1101220@nnn.ed.jp>
2022-12-26 21:07:07 +0900

    add helmet

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet({ contentSecurityPolicy: false }));
+// app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
